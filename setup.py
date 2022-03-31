4a353ca8cdc2b0ebe715aedc9b302f73149cba06
Michael Schilling <m.schilling@fz-juelich.de>
2022-03-31 15:23:43 +0200

    automatically generated git update

--- setup.py
+++ setup.py
@@ -1,15 +1,23 @@
-from distutils.core import setup
-setup(
-  name = 'mrange',         # How you named your package folder (MyLib)
-  packages = ['mrange'],   # Chose the same as "name"
-  version = '0.5',      # Start with a small number and increase it with every change you make
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'Reduce nested for i in range(n) loops to a single line',   # Give a short description about your library
-  author = 'Michael Schilling',                   # Type in your name
-  author_email = 'michael@ntropic.de',      # Type in your E-Mail
-  url = 'https://github.com/Ntropic',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/Ntropic/mrange/archive/refs/tags/v0.3.tar.gz',    # I explain this later on
-  keywords = ['nested', 'loops'],   # Keywords that define your package best
-  install_requires=[ 'numba',
-      ],
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = "mrange",
+    version = "0.6",
+    author = "Michael Schilling",
+    author_email = "michael@ntropic.de",
+    description  = "test",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/Ntropic/mrangearchive/refs/tags/v0.6.tar.gz",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where=""),
+    python_requires=">=3.6",
 )
