dfb23948e8fd54951fddb98b5163d7dffb8e0919
Michael Schilling <m.schilling@fz-juelich.de>
2022-03-31 15:17:23 +0200

    automatically generated git update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'mrange',         # How you named your package folder (MyLib)
   packages = ['mrange'],   # Chose the same as "name"
-  version = '0.4',      # Start with a small number and increase it with every change you make
+  version = '0.5',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Reduce nested for i in range(n) loops to a single line',   # Give a short description about your library
   author = 'Michael Schilling',                   # Type in your name
